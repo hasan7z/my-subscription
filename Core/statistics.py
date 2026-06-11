4d7f74ad68bce37d5325fd0a57097deb7fe6807d
Trova.Officer <45694710+hasanzamari@users.noreply.github.com>
2026-06-11 08:42:26 +0330

    statistics.py

---
+++ Core/statistics.py
@@ -0,0 +1,21 @@
+def build(
+    total,
+    valid,
+    unique,
+    new,
+    expired
+):
+
+    return {
+
+        "total": total,
+
+        "valid": valid,
+
+        "unique": unique,
+
+        "new": new,
+
+        "expired": expired
+
+    }
