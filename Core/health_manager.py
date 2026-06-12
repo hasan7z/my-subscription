843dc2235fed881c04a1437f2d19b0d5ccf566c5
Trova.Officer <45694710+hasanzamari@users.noreply.github.com>
2026-06-12 10:14:45 +0330

    health_manager.py

--- Core/health_manager.py
+++ Core/health_manager.py
@@ -10,17 +10,33 @@ def load():
         return {}
 
     try:
-        with open(FILE, "r", encoding="utf-8") as f:
+
+        with open(
+            FILE,
+            "r",
+            encoding="utf-8"
+        ) as f:
+
             return json.load(f)
+
     except:
+
         return {}
 
 
 def save(data):
 
-    os.makedirs("database", exist_ok=True)
+    os.makedirs(
+        "database",
+        exist_ok=True
+    )
 
-    with open(FILE, "w", encoding="utf-8") as f:
+    with open(
+        FILE,
+        "w",
+        encoding="utf-8"
+    ) as f:
+
         json.dump(
             data,
             f,
@@ -29,18 +45,68 @@ def save(data):
         )
 
 
-def update(url, success, count, db):
+def update(
+    url,
+    success,
+    count,
+    db
+):
 
-    item = db.get(url, {})
+    item = db.get(
+        url,
+        {}
+    )
 
-    item["success"] = item.get("success", 0)
+    item["success"] = item.get(
+        "success",
+        0
+    )
 
-    item["failed"] = item.get("failed", 0)
+    item["failed"] = item.get(
+        "failed",
+        0
+    )
+
+    item["runs"] = item.get(
+        "runs",
+        0
+    )
+
+    item["last_configs"] = item.get(
+        "last_configs",
+        0
+    )
+
+    item["runs"] += 1
 
     if success:
+
         item["success"] += 1
         item["last_configs"] = count
+
     else:
+
         item["failed"] += 1
 
+    total = (
+        item["success"]
+        +
+        item["failed"]
+    )
+
+    if total == 0:
+
+        item["rate"] = 0
+
+    else:
+
+        item["rate"] = round(
+            item["success"]
+            * 100
+            / total,
+            2
+        )
+
     db[url] = item
+
+    return db
